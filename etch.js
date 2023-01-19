eda6fd1d0d54f49b3b562994d8d748455a9a93fe
VisforVirus <117862440+VisforVirus@users.noreply.github.com>
2023-01-19 17:56:50 -0500

    fixed the grid gernation to allow for correct placements

--- etch.js
+++ etch.js
@@ -1,22 +1,23 @@
-function createGrid(contain,squares) {
-
-    const newDivContainer = document.createElement('div');
+function createGrid(contain,className) { 
+    const cont = document.querySelector(contain);
     const newDiv = document.createElement('div');
+    newDiv.classList.add(className);
     
-    newDivContainer.classList.add('gridSquareContainer');
-    newDiv.classList.add('gridSquare');
-    
-    newDivContainer.appendChild(newDiv);
-    contain.appendChild(newDivContainer);
-    
+    cont.appendChild(newDiv);
+}
+
+function gridX(contain,className,squares) {
+    const cont = document.querySelectorAll(contain);
+    const newDiv = document.createElement('div');
+    newDiv.classList.add(className);
+
+    cont[squares].appendChild(newDiv);
+    console.log(cont[squares]);
 
 }
 
-
 const button = document.querySelector('button');
 const container = document.querySelector('#container');
-
-console.log(container.firstChild)
 
 button.addEventListener('click', () => {
     const val = prompt('How many squares do you want?');
@@ -25,7 +26,12 @@ button.addEventListener('click', () => {
             container.removeChild(container.firstElementChild);
         }
     }
-    for(let i = 0; i < val; i++){
-        createGrid(container,val);
+
+    for(let q = 0; q < val; q++){
+        createGrid('#container','gridSquareContainer');
+        for (let i = 0; i < val; i++){
+            gridX('.gridSquareContainer','gridSquare',q);
+        }
     }
 })
+
